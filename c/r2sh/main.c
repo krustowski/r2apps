b33caa48d499105876707a3f2428984c68bf274c
krusty <krusty@vxn.dev>
2025-08-03 01:00:30 +0200

    Fix formatting, refactor

--- c/r2sh/main.c
+++ c/r2sh/main.c
@@ -1,6 +1,6 @@
 #include "syscall.h"
 
-int main(int64_t pid, int64_t *args)
+int main(int64_t pid, int64_t arg)
 {
 	const uint8_t *filename = "C.TXT";
 	const uint8_t *wbuffer = "Written from C\n";
@@ -9,23 +9,37 @@ int main(int64_t pid, int64_t *args)
 
 	Entry_T entries[32];
 
+	/* Test printing to standard output (console, syscall 0x10) */
 	print("*** Hello from C\n");
 
+	/* Test writing to a file (syscall 0x21) */
 	if (write_file(filename, wbuffer))
 	{
 		print("*** Written to a file\n");
 	}
 
+	/* Test reading such file back (syscall 0x20) */
 	if (read_file(filename, rbuffer))
 	{
 		print(rbuffer);
 	}
 
-	if (!list_dir(0, entries)) 
+	/* Test listing a directory (syscall 0x28) */
+	if (list_dir(0, entries)) 
 	{
-		print("*** Empty cluster\n");
+		for (uint8_t i = 0; i < 32; i++) 
+		{
+			if (entries[i].name[0] == 0x00) 
+			{
+				continue;
+			}
+
+			print(" ");
+			print( (const uint8_t*)(entries[i].name) );
+			print("\n");
+		}
 	}
 
-	exit(123, 999);
+	exit(pid, 999);
 }
 
--- c/r2sh/syscall.c
+++ c/r2sh/syscall.c
@@ -2,14 +2,14 @@
 
 int64_t syscall(int64_t number, int64_t arg1, int64_t arg2, int64_t arg3) 
 {
-    int64_t ret;
-    asm volatile (
-        "int $0x7f"
-        : "=a"(ret)
-        : "a"(number), "D"(arg1), "S"(arg2), "d"(arg3)
-        : "rcx", "r11", "memory"
-    );
-    return ret;
+	int64_t ret;
+	asm volatile (
+			"int $0x7f"
+			: "=a"(ret)
+			: "a"(number), "D"(arg1), "S"(arg2), "d"(arg3)
+			: "rcx", "r11", "memory"
+		     );
+	return ret;
 }
 
 void exit(int64_t pid, int64_t code)
@@ -27,7 +27,7 @@ int64_t print(const uint8_t *str)
 
 	if (len > 0)
 	{
-	    	if (syscall(ScPrints, (int64_t)str, len, 0))
+		if (syscall(ScPrints, (int64_t)str, len, 0))
 		{
 			return 0;
 		}
@@ -77,18 +77,6 @@ int64_t list_dir(int64_t cluster, Entry_T entries[32])
 	if (syscall(ScListDir, cluster, (int64_t)entries, 0))
 	{
 		return 0;
-	}
-
-	for (uint8_t i = 0; i < 32; i++) 
-	{
-		if (entries[i].name[0] == 0x00) 
-		{
-			continue;
-		}
-
-		print(" ");
-		print( (const uint8_t*)(entries[i].name) );
-		print("\n");
 	}
 
 	return 1;
