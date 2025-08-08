4dd3d2063cce8b670b0af3e7d3d1c34a398f7a42
krusty <krusty@vxn.dev>
2025-08-08 10:23:31 +0200

    Refactor r2sh C stub

--- c/r2sh/main.c
+++ c/r2sh/main.c
@@ -1,28 +1,5 @@
+#include "print.h"
 #include "syscall.h"
-
-void u32_to_str(uint32_t value, uint8_t *buffer) {
-	uint8_t temp[10];
-	uint32_t i = 0;
-
-	if (value == 0) {
-		buffer[0] = '0';
-		buffer[1] = '\0';
-		return;
-	}
-
-	// Convert digits in reverse order
-	while (value > 0 && i < 10) {
-		temp[i++] = '0' + (value % 10);
-		value /= 10;
-	}
-
-	// Reverse the digits into the output buffer
-	for (uint8_t j = 0; j < i; j++) {
-		buffer[j] = temp[i - j - 1];
-	}
-
-	buffer[i] = '\0';
-}
 
 int main(int64_t pid, int64_t arg)
 {
