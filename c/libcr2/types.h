8b9f38d57b9dc80555418548c0585365e7ad33f6
krusty <krusty@vxn.dev>
2025-08-10 00:56:21 +0200

    Update types header file in libcr2

--- c/libcr2/types.h
+++ c/libcr2/types.h
@@ -13,7 +13,15 @@
 extern "C" {
 #endif
 
-typedef long int64_t;
+typedef enum {
+	false,
+	true
+} bool;
+
+typedef char 	int8_t;
+typedef short 	int16_t;
+typedef int  	int32_t;
+typedef long 	int64_t;
 
 typedef unsigned char 	uint8_t;
 typedef unsigned short 	uint16_t;
