ad6ff919fbd4b31a0b32027e5297f03e3dc2840c
krusty <krusty@vxn.dev>
2025-08-10 22:25:17 +0200

    Make socket list a local var in main function

--- c/garn/main.c
+++ c/garn/main.c
@@ -11,12 +11,15 @@
  *  krusty@vxn.dev / Aug 5, 2025
  */
 
+#define BIND_PORT	80 
+
 int main(int64_t pid, int64_t arg)
 {
 	uint32_t value = 0;
 
-	uint8_t temp_buf[2048];
-	uint8_t packet_buf[2048];
+	static uint8_t temp_buf[2048];
+	static uint8_t packet_buf[2048];
+
 	uint8_t temp_len = 0;
 	int64_t decoded_len = 0;
 
@@ -27,8 +30,11 @@ int main(int64_t pid, int64_t arg)
 	uint8_t tcp_packet[1500];
 	uint8_t tcp_header_len = 0;
 
-	TcpSocket_T *server = socket_tcp4();
-	bind(server, 80);
+	TcpSocket_T sockets[MAX_SOCKETS];
+
+	// Start the server
+	TcpSocket_T *server = socket_tcp4(sockets);
+	bind(server, BIND_PORT);
 	listen(server);
 
 	print("-> garn HTTP service starting up...\n");
@@ -79,13 +85,13 @@ int main(int64_t pid, int64_t arg)
 
 		printf(">> TCP: src_port: %u, dest_port: %u, seq %u\n", tcp_header.source_port, tcp_header.dest_port, tcp_header.seq_num);
 
-		on_tcp_packet(ipv4_header.source_addr, ipv4_header.destination_addr, &tcp_header, tcp_packet, decoded_len - ipv4_header_len);
+		on_tcp_packet(ipv4_header.source_addr, ipv4_header.destination_addr, &tcp_header, tcp_packet, decoded_len - ipv4_header_len, sockets);
 
 		//
 		//
 		//
 
-		TcpSocket_T *client = accept(server);
+		TcpSocket_T *client = accept(server, sockets);
 
 		if (!client)
 		{
