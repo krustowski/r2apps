34e01151d6ca12d3bfb4d993ffc7e326183e9f2c
krusty <krusty@vxn.dev>
2025-08-11 03:43:07 +0200

    Revert the formatting layout

--- c/r2sh/main.c
+++ c/r2sh/main.c
@@ -1,43 +1,54 @@
 #include "printf.h"
 #include "syscall.h"
 
-int main(int64_t pid, int64_t arg) {
+int main(int64_t pid, int64_t arg)
+{
 	uint8_t buf[17];
 	uint8_t cap = sizeof(buf);
 	uint8_t head = 15;
 	uint8_t tail = 15;
 
-	for (uint8_t i = 0; i < sizeof(buf); i++) {
+	for (uint8_t i = 0; i < sizeof(buf); i++)
+	{
 		buf[i] = 0x00;
 	}
 
-	if (!pipe_subscribe((const uint8_t *) buf)) {
+	if (!pipe_subscribe((const uint8_t *) buf))
+	{
 		print("-> Buffer not subscribed!\n");
 	}
 
-	while (3) {
-		while (buf[0]) {
-			buf[head--] = buf[0];
+	for (;;)
+	{
+		while (buf[0])
+		{
+			buf[head] = buf[0];
 			buf[0] = 0x00;
 
-			if (head == 1) {
+			if (--head == 1)
+			{
 				head = 15;
 			}
 		}
 
-		do {
-			if (buf[tail]) {
+		do
+		{
+			if (buf[tail])
+			{
 				printf("%x", buf[tail]);
 				buf[tail] = 0x00;
 			}
 
-			if (--tail == 1) {
+			if (--tail == 1)
+			{
 				tail = 15;
 			}
-		} while (tail > head);
+		} 
+		while (tail > head);
 	}
 
-	if (!pipe_unsubscribe((const uint8_t *) buf)) {
+	if (!pipe_unsubscribe((const uint8_t *) buf))
+	{
 		print("-> Buffer not unsubscribed!\n");
 	}
 
