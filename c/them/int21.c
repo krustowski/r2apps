4de22be470574ffb6051153338e7b4c26a0bc541
krusty <krusty@vxn.dev>
2025-11-04 13:41:36 +0100

    make an enumeration of int 21h DOS services

--- c/them/int21.c
+++ c/them/int21.c
@@ -1,30 +1,28 @@
 #include "printf.h"
 
 #include "cpu.h"
+#include "int.h"
 
 void int21h(CPU_T* cpu, uint8_t* memory)
 {
-	if (cpu->AX >> 8 == 0x09)
+	enum SRV_21H service = cpu->AX >> 8;
+
+	switch (service)
 	{
-		uint8_t offset = cpu->DX & 0xff;
-		uint8_t segment = cpu->DS & 0xff;
+		case CHAR_STRING_OUTPUT:
+			{
+				uint8_t offset = cpu->DX & 0xff;
+				uint8_t segment = cpu->DS & 0xff;
 
-		// Linear addr in Real mode
-		uint16_t addr = ((uint16_t)segment << 4) + offset;
+				/* Linear addr in Real mode */
+				uint16_t addr = ((uint16_t)segment << 4) + offset;
 
-		uint8_t *p = &memory[addr];
+				uint8_t *p = &memory[addr];
 
-		printf("=> INT 21H: printing string from addr: %x\n", addr);
+				/*printf("=> INT 21H: printing string from addr: %x\n", addr);*/
 
-		/*while (*p != '$')
-		{
-			print(p);
-			p++;
-		}*/
-
-		print(p);
-		print("\n");
+				print(p);
+				print("\n");
+			}
 	}
 }
-
-
