18ff9f1f408ab99654e834a9c491d76a229f83c2
krusty <krusty@vxn.dev>
2025-11-09 01:04:34 +0100

    Use crt0 in the build process

---
+++ c/them/README.md
@@ -0,0 +1,13 @@
+# theM: the 16-bit CPU & DOS emulator
+
+## Usage
+
+```shell
+make build
+```
+
+```r2
+run THEM
+```
+
+Ensure both `THEM.ELF` and `PRG0.BIN` files are in the same directory and accessive.
--- c/them/cpu.c
+++ c/them/cpu.c
@@ -2,9 +2,9 @@
 #include "printf.h"
 
 void dump_registers(CPU_T *cpu) {
-    printf((const uint8_t *)"\n[ CPU Registers: ]\n");
-    printf((const uint8_t *)"[ AX: %x, BX: %x, CX: %x, DX: %x, SP: %x, BP: %x, "
-                            "SI: %x, DI: %x ]\n",
-           cpu->AX, cpu->BX, cpu->CX, cpu->DX, cpu->SP, cpu->BP, cpu->SI, cpu->DI);
-    printf((const uint8_t *)"[ CS: %x, DS: %x, ES: %x, SS: %x, IP: %x ]\n", cpu->CS, cpu->DS, cpu->ES, cpu->SS, cpu->IP);
+	printf((const uint8_t *)"\n[ CPU Registers: ]\n");
+	printf((const uint8_t *)"[ AX: %x, BX: %x, CX: %x, DX: %x, SP: %x, BP: %x, SI: %x, DI: %x ]\n",
+			cpu->AX, cpu->BX, cpu->CX, cpu->DX, cpu->SP, cpu->BP, cpu->SI, cpu->DI);
+	printf((const uint8_t *)"[ CS: %x, DS: %x, ES: %x, SS: %x, IP: %x ]\n", 
+			cpu->CS, cpu->DS, cpu->ES, cpu->SS, cpu->IP);
 }
