234661904ee1dc46c1e2ff94e23f3f114d26a9bd
krusty <krusty@vxn.dev>
2025-11-21 13:38:18 +0100

    Refactor c/r2sh

--- c/r2sh/main.c
+++ c/r2sh/main.c
@@ -1,57 +1,45 @@
 #include "printf.h"
 #include "syscall.h"
 
-int main(int64_t pid, int64_t arg)
-{
-	uint8_t buf[17];
-	uint8_t cap = sizeof(buf);
-	uint8_t head = 15;
-	uint8_t tail = 15;
+int main(void) {
+    uint8_t buf[17];
+    uint8_t cap = sizeof(buf);
+    uint8_t head = 15;
+    uint8_t tail = 15;
 
-	for (uint8_t i = 0; i < sizeof(buf); i++)
-	{
-		buf[i] = 0x00;
-	}
+    for (uint8_t i = 0; i < sizeof(buf); i++) {
+        buf[i] = 0x00;
+    }
 
-	if (!pipe_subscribe((const uint8_t *) buf))
-	{
-		print("-> Buffer not subscribed!\n");
-	}
+    if (!pipe_subscribe((const uint8_t *)buf)) {
+        print((const uint8_t *)"-> Buffer not subscribed!\n");
+    }
 
-	for (;;)
-	{
-		while (buf[0])
-		{
-			buf[head] = buf[0];
-			buf[0] = 0x00;
+    for (;;) {
+        while (buf[0]) {
+            buf[head] = buf[0];
+            buf[0] = 0x00;
 
-			if (--head == 1)
-			{
-				head = 15;
-			}
-		}
+            if (--head == 1) {
+                head = 15;
+            }
+        }
 
-		do
-		{
-			if (buf[tail])
-			{
-				printf("%x", buf[tail]);
-				buf[tail] = 0x00;
-			}
+        do {
+            if (buf[tail]) {
+                printf((const uint8_t *)"%x", buf[tail]);
+                buf[tail] = 0x00;
+            }
 
-			if (--tail == 1)
-			{
-				tail = 15;
-			}
-		} 
-		while (tail > head);
-	}
+            if (--tail == 1) {
+                tail = 15;
+            }
+        } while (tail > head);
+    }
 
-	if (!pipe_unsubscribe((const uint8_t *) buf))
-	{
-		print("-> Buffer not unsubscribed!\n");
-	}
+    if (!pipe_unsubscribe((const uint8_t *)buf)) {
+        print((const uint8_t *)"-> Buffer not unsubscribed!\n");
+    }
 
-	exit(pid, 345);
+    return 0;
 }
-
