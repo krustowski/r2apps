32a4f0901237c92a57b58775e5b956f0498dd37c
krusty <krusty@vxn.dev>
2026-04-24 14:15:09 +0200

    Fix formatting

--- c/libcr2/net.h
+++ c/libcr2/net.h
@@ -4,7 +4,7 @@
 /*
  *  net.h
  *
- *  Custom C header file providing prototypes, definitions and constants to 
+ *  Custom C header file providing prototypes, definitions and constants to
  *  work with netwoking via the r2 kernel project.
  *
  *  krusty@vxn.dev / Aug 8, 2025
@@ -22,33 +22,33 @@ extern "C" {
  *
  *  Definitions related to the decoder.
  */
-#define SLIP_END     0xC0
-#define SLIP_ESC     0xDB
+#define SLIP_END 0xC0
+#define SLIP_ESC 0xDB
 #define SLIP_ESC_END 0xDC
 #define SLIP_ESC_ESC 0xDD
 
 /*
  *  TCP socket-related definitions
  */
-#define MAX_SOCKETS	8
-#define RX_BUFFER_SIZE  1024
-#define TX_BUFFER_SIZE  1024
+#define MAX_SOCKETS 8
+#define RX_BUFFER_SIZE 1024
+#define TX_BUFFER_SIZE 1024
 
-#define TCP_FLAG_FIN	0x01
-#define TCP_FLAG_SYN	0x02
-#define TCP_FLAG_RST	0x04
-#define TCP_FLAG_ACK	0x10
+#define TCP_FLAG_FIN 0x01
+#define TCP_FLAG_SYN 0x02
+#define TCP_FLAG_RST 0x04
+#define TCP_FLAG_ACK 0x10
 
-#define CRAFT_IPV4_PACKET 	0x01
-#define CRAFT_ICMP_PACKET 	0x02
-#define CRAFT_TCP_PACKET 	0x03
+#define CRAFT_IPV4_PACKET 0x01
+#define CRAFT_ICMP_PACKET 0x02
+#define CRAFT_TCP_PACKET 0x03
 
 /* Raw Ethernet frame type code for receive_data() / send_eth_frame() */
-#define RECV_ETH   0x04
+#define RECV_ETH 0x04
 
 /* Ethertype constants */
 #define ETYPE_IPV4 0x0800
-#define ETYPE_ARP  0x0806
+#define ETYPE_ARP 0x0806
 
 /*
  *  type EthHdr_T structure
@@ -56,8 +56,8 @@ extern "C" {
  *  Standard 14-byte Ethernet II frame header.
  */
 typedef struct {
-    uint8_t  dst[6];
-    uint8_t  src[6];
+    uint8_t dst[6];
+    uint8_t src[6];
     uint16_t ethertype; /* big-endian */
 } __attribute__((packed)) EthHdr_T;
 
@@ -69,13 +69,13 @@ typedef struct {
 typedef struct {
     uint16_t htype; /* 1 = Ethernet */
     uint16_t ptype; /* 0x0800 = IPv4 */
-    uint8_t  hlen;  /* 6 */
-    uint8_t  plen;  /* 4 */
+    uint8_t hlen;   /* 6 */
+    uint8_t plen;   /* 4 */
     uint16_t oper;  /* 1 = request, 2 = reply */
-    uint8_t  sha[6];
-    uint8_t  spa[4];
-    uint8_t  tha[6];
-    uint8_t  tpa[4];
+    uint8_t sha[6];
+    uint8_t spa[4];
+    uint8_t tha[6];
+    uint8_t tpa[4];
 } __attribute__((packed)) ArpPkt_T;
 
 #define ETH_HDR_LEN ((uint32_t)sizeof(EthHdr_T))
@@ -89,7 +89,7 @@ typedef struct {
  *  startup without touching the TCP/IP logic.
  */
 typedef struct {
-    int  (*recv)(uint8_t *buf, uint32_t maxlen);
+    int (*recv)(uint8_t *buf, uint32_t maxlen);
     void (*send_ip)(const uint8_t *ip_pkt, uint32_t len);
 } NetDriver_T;
 
@@ -110,16 +110,16 @@ int net_driver_select(const uint8_t *name);
  *  This structure specifies the property list of an IPv4 header.
  */
 typedef struct {
-	uint8_t version;
-	uint8_t dscp_ecn;
-	uint16_t total_length;
-	uint16_t identification;
-	uint16_t flags_fragment_offset;
-	uint8_t ttl;
-	uint8_t protocol;
-	uint16_t header_checksum;
-	uint8_t source_addr[4];
-	uint8_t destination_addr[4];
+    uint8_t version;
+    uint8_t dscp_ecn;
+    uint16_t total_length;
+    uint16_t identification;
+    uint16_t flags_fragment_offset;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t header_checksum;
+    uint8_t source_addr[4];
+    uint8_t destination_addr[4];
 } __attribute__((packed)) Ipv4Header_T;
 
 /*
@@ -128,11 +128,11 @@ typedef struct {
  *  This structure specifies the property list of an ICMP header.
  */
 typedef struct {
-	uint8_t type;
-	uint8_t code;
-	uint16_t checksum;
-	uint16_t identifier;
-	uint16_t sequence_number;
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
+    uint16_t identifier;
+    uint16_t sequence_number;
 } __attribute__((packed)) IcmpHeader_T;
 
 /*
@@ -141,14 +141,14 @@ typedef struct {
  *  This structure specifies the property list of a TCP header.
  */
 typedef struct {
-	uint16_t source_port;
-	uint16_t dest_port;
-	uint32_t seq_num;
-	uint32_t ack_num;
-	uint16_t data_offset_reserved_flags;
-	uint16_t window_size;
-	uint16_t checksum;
-	uint16_t urgent_pointer;
+    uint16_t source_port;
+    uint16_t dest_port;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    uint16_t data_offset_reserved_flags;
+    uint16_t window_size;
+    uint16_t checksum;
+    uint16_t urgent_pointer;
 } __attribute__((packed)) TcpHeader_T;
 
 /*
@@ -158,10 +158,10 @@ typedef struct {
  *  with a TCP header when requesting a new TCP packet from r2 kernel.
  */
 typedef struct {
-	TcpHeader_T header;
-	uint8_t src_ip[4];
-	uint8_t dst_ip[4];
-	uint16_t length;
+    TcpHeader_T header;
+    uint8_t src_ip[4];
+    uint8_t dst_ip[4];
+    uint16_t length;
 } __attribute__((packed)) TcpPacketRequest_T;
 
 /*
@@ -169,34 +169,28 @@ typedef struct {
  *
  *  Uncomplete list of various TCP connection states.
  */
-typedef enum {
-	SOCKET_CLOSED,
-	SOCKET_LISTENING,
-	SOCKET_SYN_SENT,
-	SOCKET_ESTABLISHED,
-	SOCKET_FIN_WAIT
-} SocketState;
+typedef enum { SOCKET_CLOSED, SOCKET_LISTENING, SOCKET_SYN_SENT, SOCKET_ESTABLISHED, SOCKET_FIN_WAIT } SocketState;
 
 /*
  *  type TcpSocket_T structure
  *
- *  TCP connection implementing socket strucutre used to track the connection state, 
+ *  TCP connection implementing socket strucutre used to track the connection state,
  *  properties and stuff.
  */
 typedef struct TcpSocket_T {
-	uint32_t id;
-	SocketState state;
-	uint16_t local_port;
-	uint16_t remote_port;
-	uint8_t local_ip[4];
-	uint8_t remote_ip[4];
-	uint8_t rx_buffer[RX_BUFFER_SIZE];
-	uint8_t tx_buffer[TX_BUFFER_SIZE];
-	uint32_t rx_len;
-	uint32_t tx_len;
-	uint8_t used;
-	uint32_t seq_num;
-	uint32_t ack_num;
+    uint32_t id;
+    SocketState state;
+    uint16_t local_port;
+    uint16_t remote_port;
+    uint8_t local_ip[4];
+    uint8_t remote_ip[4];
+    uint8_t rx_buffer[RX_BUFFER_SIZE];
+    uint8_t tx_buffer[TX_BUFFER_SIZE];
+    uint32_t rx_len;
+    uint32_t tx_len;
+    uint8_t used;
+    uint32_t seq_num;
+    uint32_t ack_num;
 } __attribute__((packed)) TcpSocket_T;
 
 /*
@@ -213,7 +207,7 @@ TcpSocket_T *tcp_connect(TcpSocket_T sockets[MAX_SOCKETS], const uint8_t remote_
  *
  *  This function should be capeble of send the TCP packet wrapped in an IPv4 packet successfully over the line.
  */
-void send_tcp_packet(TcpSocket_T *sock, const uint8_t* data, uint32_t len, uint8_t flags);
+void send_tcp_packet(TcpSocket_T *sock, const uint8_t *data, uint32_t len, uint8_t flags);
 
 /*
  *  TcpSocket_T *socket_tcp4() prototype
@@ -253,7 +247,7 @@ void listen(TcpSocket_T *sock);
 /*
  *  TcpSocket_T *accept() prototype
  *
- *  This function checks the array of sockets and returns the one marked as used, with the conn state as ESTABLISHED, 
+ *  This function checks the array of sockets and returns the one marked as used, with the conn state as ESTABLISHED,
  *  and wuth the <local_port> being the same as the listener's one.
  */
 TcpSocket_T *accept(TcpSocket_T *listener, TcpSocket_T sockets[MAX_SOCKETS]);
@@ -276,7 +270,7 @@ uint32_t write(TcpSocket_T *sock, const uint8_t *buf, uint32_t len);
 /*
  *  void close() prototype
  *
- *  This function closes the connection tracked by such socket provided. 
+ *  This function closes the connection tracked by such socket provided.
  */
 void close(TcpSocket_T *sock);
 
@@ -300,7 +294,7 @@ int64_t decode_slip(const uint8_t *input, uint32_t input_len, uint8_t *output, u
 /*
  *  uint16_t parse_ipv4_packet() prototype
  *
- *  A simple macro-like function that copyies the contents of the packet into the IPv4 
+ *  A simple macro-like function that copyies the contents of the packet into the IPv4
  *  header structure of declared size. Function returns the header size.
  */
 uint16_t parse_ipv4_packet(const uint8_t *packet, Ipv4Header_T *header);
